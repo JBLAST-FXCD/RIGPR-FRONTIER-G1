d4cafaf3365856cd27b3f0093412a7f56c77e508
up2210635 <up2210635@myport.ac.uk>
2025-12-05 09:46:58 +0000

    ParetBuilding
    Started coding the parent building class.
    
    Created a temp mouse class to code functionality in the parent building code without errors.
    
    So far, you can only select the correct tier of building; everything else needs to be improved.

---
+++ Mousechitect/Assets/Features/Buildings/Scripts/MouseTemp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTemp : MonoBehaviour
+{
+    [SerializeField] private GameObject building_prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Mousechitect/Assets/Features/Buildings/Scripts/ParentBuilding.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parent_building : MonoBehaviour
+{
+    [SerializeField] private GameObject[] building_prefabs;
+    [SerializeField] private int[]        capacitys;
+    [SerializeField] private int          tier;
+
+    private GameObject building_prefab;
+    private List<MouseTemp> Mouse;
+    private int capacity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConstructTier(tier);
+        building_prefab.transform.localPosition = this.transform.localPosition;
+        Instantiate(building_prefab);
+    }
+
+    // Pick and the diffrenmt mesh and stats for the teirs
+    private void ConstructTier(int tier) 
+    {
+        tier--;
+        building_prefab = building_prefabs[tier];
+        capacity = capacitys[tier];
+    }
+
+    private void OnTriggerEnter(MouseTemp other)
+    {
+
+        if (other != null && Mouse.Count <= capacity)
+        {
+            other.transform.gameObject.SetActive(false);
+            Mouse.Add(other);
+        }
+    }
+
+    private void MouseLeave(MouseTemp m1)
+    {
+        Vector3 new_loc = m1.transform.localPosition;
+
+        switch (this.transform.eulerAngles.y)
+        {
+            case 0:
+                new_loc.z += 1;
+                break;
+            case 90:
+                new_loc.x += 1;
+                break;
+            case 180:
+                new_loc.z -= 1;
+                break;
+            case 270:
+                new_loc.x -= 1;
+                break;
+        }
+
+        m1.transform.localPosition = new_loc;
+        m1.transform.gameObject.SetActive(true);
+    }
+}
