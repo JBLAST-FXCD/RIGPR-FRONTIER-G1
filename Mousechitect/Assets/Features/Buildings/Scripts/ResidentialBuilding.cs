ccca7e19f0432be24d9d87ac2f92be38a55fab26
up2210635 <up2210635@myport.ac.uk>
2026-01-23 12:00:42 +0000

    Level and Morale
    Finish designing the level for testing and add changes to the building class, which I implemented in Anothy's branch to stop it from being overwritten.

--- Mousechitect/Assets/Features/Buildings/Scripts/ResidentialBuilding.cs
+++ Mousechitect/Assets/Features/Buildings/Scripts/ResidentialBuilding.cs
@@ -13,15 +13,13 @@ public class ResidentialBuilding : ParentBuilding
 
     protected int quality;
 
-    public ResidentialBuilding()
-    {
-        quality = 0;
-    }
+    public int Quality { get { return quality; } }
+    public int[] Max_quality { get { return max_quality; } }
 
     protected new void TierSelection()
     {
         building_prefab = building_prefabs[tier - 1];
-        capacity        = capacitys[tier - 1];
-        quality         = max_quality[tier - 1];
+        capacity = capacitys[tier - 1];
+        quality = max_quality[tier - 1];
     }
 }
