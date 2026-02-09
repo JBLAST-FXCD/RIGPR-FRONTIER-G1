c9ae01399d4a4fedbdd9d27c987da3c996ac32d7
UnknownGamer925 <jdmdev_official@outlook.com>
2026-02-09 13:28:26 +0000

    Added building categories
    Added functionality on the BuildPanel to navigate between categories and for the building buttons to be visible only when relevant to the current category.
    
    Additionally, moved the NewUserInterfaceManager script onto NUI Canvas to consolidate systems ready for prefabbing.

--- Mousechitect/Assets/Features/NUI/NewUserInterfaceManager.cs
+++ Mousechitect/Assets/Features/NUI/NewUserInterfaceManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.Animations;
 using UnityEngine;
 
@@ -7,12 +9,29 @@ public class NewUserInterfaceManager : MonoBehaviour
 {
 
     [Header("Panels")]
-    [SerializeField] private GameObject build_panel;
+    
     [SerializeField] private GameObject path_panel;
+
+    [Header("Build Panel")]
+    [SerializeField] private GameObject build_panel;
+    [SerializeField] private TextMeshProUGUI category_text;
+    [SerializeField] private GameObject[] residential_rows;
+    [SerializeField] private GameObject[] industrial_rows;
+    [SerializeField] private GameObject[] commercial_rows;
+    [SerializeField] private GameObject[] research_rows;
+
 
     [Header("Systems")]
     [SerializeField] private BuildingManager building_manager;
 
+    enum CATEGORY
+    {
+        CATEGORY_RESIDENTIAL,
+        CATEGORY_INDUSTRIAL,
+        CATEGORY_COMMERCIAL,
+        CATEGORY_RESEARCH,
+    }
+    private CATEGORY current_category = CATEGORY.CATEGORY_RESIDENTIAL;
 
     enum OPEN_PANEL
     {
@@ -91,5 +110,98 @@ public class NewUserInterfaceManager : MonoBehaviour
             path_panel.GetComponent<Animator>().SetBool("is_panel_open", true);
             open_panel = OPEN_PANEL.OPEN_PANEL_PATH;
         }
+    }
+
+    public void BuildCategoryNext()
+    {
+        CATEGORY previous_category = current_category;
+        switch (current_category)
+        {
+            case CATEGORY.CATEGORY_RESIDENTIAL:
+                current_category = CATEGORY.CATEGORY_INDUSTRIAL;
+                category_text.text = "Industrial";
+                break;
+            case CATEGORY.CATEGORY_INDUSTRIAL:
+                current_category = CATEGORY.CATEGORY_COMMERCIAL;
+                category_text.text = "Commerical";
+                break;
+            case CATEGORY.CATEGORY_COMMERCIAL:
+                current_category = CATEGORY.CATEGORY_RESEARCH;
+                category_text.text = "Research";
+                break;
+            case CATEGORY.CATEGORY_RESEARCH:
+                current_category = CATEGORY.CATEGORY_RESIDENTIAL;
+                category_text.text = "Residential";
+                break;
+            default:
+                current_category = CATEGORY.CATEGORY_RESIDENTIAL;
+                category_text.text = "Error";
+                break;
+        }
+        UpdateCategory(previous_category);
+    }
+
+    public void BuildCategoryPrevious()
+    {
+        CATEGORY previous_category = current_category;
+        switch (current_category)
+        {
+            case CATEGORY.CATEGORY_RESEARCH:
+                current_category = CATEGORY.CATEGORY_COMMERCIAL;
+                category_text.text = "Commerical";
+                break;
+            case CATEGORY.CATEGORY_COMMERCIAL:
+                current_category = CATEGORY.CATEGORY_INDUSTRIAL;
+                category_text.text = "Industrial";
+                break;
+            case CATEGORY.CATEGORY_INDUSTRIAL:
+                current_category = CATEGORY.CATEGORY_RESIDENTIAL;
+                category_text.text = "Residential";
+                break;
+            case CATEGORY.CATEGORY_RESIDENTIAL:
+                current_category = CATEGORY.CATEGORY_RESEARCH;
+                category_text.text = "Research";
+                break;
+            default:
+                current_category = CATEGORY.CATEGORY_RESIDENTIAL;
+                category_text.text = "Error";
+                break;
+        }
+        UpdateCategory(previous_category);
+    }
+
+    private GameObject[] LoadBuildArray(CATEGORY load_category)
+    {
+        switch(load_category)
+        {
+            case CATEGORY.CATEGORY_RESIDENTIAL:
+                return residential_rows;
+            case CATEGORY.CATEGORY_INDUSTRIAL:
+                return industrial_rows;
+            case CATEGORY.CATEGORY_COMMERCIAL:
+                return commercial_rows;
+            case CATEGORY.CATEGORY_RESEARCH:
+                return research_rows;
+            default:
+                return residential_rows;
+        }
+    }
+
+    private void UpdateCategory(CATEGORY previous)
+    {
+        GameObject[] selected_rows = LoadBuildArray(previous);
+
+        for (int i = 0; i < selected_rows.Length; i++)
+        {
+            selected_rows[i].SetActive(false);
+        }
+
+        selected_rows = LoadBuildArray(current_category);
+
+        for (int i = 0; i < selected_rows.Length; i++)
+        {
+            selected_rows[i].SetActive(true);
+        }
+
     }
 }
